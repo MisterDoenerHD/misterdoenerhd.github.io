ed29507eaba27d140192ffd2fdd32f7a0bfad871
Jonas Franke <90050350+JonasFranke@users.noreply.github.com>
2022-01-21 10:25:32 +0100

    add fn

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-function buttonclick(){
-    window.location.href = "basket-random.html";
+function buttonclick(seite){
+    switch(seite) {
+        case "basket":
+            window.location.href = "basket-random.html";
+            break;
+        case "football":
+            window.location.href = "football-random.html";
+            break;
+        case "gun":
+            window.location.href = "gun-mayhem.html";
+            break;
+        case "yt":
+            window.location.href = "youtube.html";
+            break;
+        case "volley":
+            window.location.href = "volley-random.html";
+            break;
+        case "roof":
+            window.location.href = "rooftop-snipers.html";
+            break;
+    }
+    
 }
