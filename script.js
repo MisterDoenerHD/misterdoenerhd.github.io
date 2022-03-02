71e133fdbcf2b33aa254bb9eaa13152a9482aea3
MisterDoenerHD <77788078+MisterDoenerHD@users.noreply.github.com>
2022-03-02 11:49:18 +0100

    add minesweeper (no embed)

--- script.js
+++ script.js
@@ -42,6 +42,9 @@ function buttonclick(seite){
         case "quiz":
             window.location.href = "quiz.html";
             break;
+        case "mine":
+            window.location.href = "https://minesweeper-online.de/index.html/";
+            break;
     }
     
 }
